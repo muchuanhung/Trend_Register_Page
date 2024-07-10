95d64c3274c3bb5d70fd6f409cbd37fd79c86554
MuChuan Hung <61078091+muchuanhung@users.noreply.github.com>
2024-07-10 11:05:42 +0800

    build: init Register template & environment setup

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Trend_Register_Page
 React Google &amp; Facebook Login Page
+
+This is a responsive register page built with React.
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/muchuanhung/Trend_Register_Page.git
+cd Trend_register_page
---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './RegisterForm.css';
+
+const RegisterForm = () => {
+  return (
+    <div className="container">
+      <h1>Register</h1>
+      <form id="registerForm">
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterForm;
